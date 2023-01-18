ba0933d0962180649ea5106ddc943ab9342c000d
Alex Akhtyrtsev <a.akhtyrtsev@gmail.com>
2023-01-18 17:45:42 +0200

    Refactor code.

--- index.ts
+++ index.ts
@@ -1,15 +1,6 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import {app} from './app/HttpServer'
+import './app/WebSocketServer'
 
-dotenv.config();
-
-const app: Express = express();
-const port = process.env.PORT;
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
-
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`⚡️[server]: Server is running at port ${process.env.PORT}`);
 });
