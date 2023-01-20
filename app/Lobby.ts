8d49e8c9076c8e60e85998facf4b583a86fd8ac9
Alex Akhtyrtsev <a.akhtyrtsev@gmail.com>
2023-01-20 23:47:19 +0200

    Add Lobby class.

---
+++ app/Lobby.ts
@@ -0,0 +1,28 @@
+import {WebSocket} from "ws";
+
+export default class Lobby {
+    static id = 0;
+
+    private participants: WebSocket[] = [];
+    public readonly id = ++Lobby.id;
+    constructor(public readonly limit = 2) {}
+
+    add(participant: WebSocket) {
+        if (this.isFull)
+            return;
+
+        this.participants.push(participant);
+    }
+
+    remove(participant: WebSocket) {
+        this.participants = this.participants.filter( p => p !== participant )
+    }
+
+    get isFull() {
+        return this.limit === this.participants.length;
+    }
+
+    public participantList() {
+        return [...this.participants];
+    }
+}
