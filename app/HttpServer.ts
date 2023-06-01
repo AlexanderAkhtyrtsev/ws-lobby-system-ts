0f47bbb317a9b09765d08d52a256ef09968bdafb
Alex Akhtyrtsev <a.akhtyrtsev@gmail.com>
2023-06-01 19:23:32 +0300

    Fix responses.

--- app/HttpServer.ts
+++ app/HttpServer.ts
@@ -2,15 +2,21 @@ import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import http from "http";
 import Lobby from "./Lobby";
+import path from "path";
 
 dotenv.config();
 
 export const app: Express = express();
 
-app.post('/lobby', () => {
+app.get('/', (req, res) => {
+   res.sendFile(path.resolve(__dirname, '../public/index.html'))
+});
+
+app.post('/lobby', (req, res) => {
     const lobby = new Lobby()
 
     console.log(`Lobby #${lobby.id} created`)
+    res.send({id: lobby.id})
 })
 
 export const server = http.createServer(app);
