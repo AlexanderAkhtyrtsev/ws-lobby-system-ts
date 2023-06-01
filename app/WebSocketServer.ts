94f813fcce7d293f7e993a0a75597d73e8c86f4a
Alex Akhtyrtsev <a.akhtyrtsev@gmail.com>
2023-06-01 19:46:47 +0300

    Fix getting lobby by id.

--- app/WebSocketServer.ts
+++ app/WebSocketServer.ts
@@ -3,16 +3,26 @@ import {app, server} from "./HttpServer";
 import Lobby, {LobbyEvents} from "./Lobby";
 import {createPackage, PackageTypes} from "./WebsocketPackage";
 
-app.get('/ws/:id', ( req, res ) => {
-    const { id } = req.params;
-
+app.get('/ws', ( req, res ) => {
     const wss = new WebSocket.Server({ server });
 
     wss.on('connection', (ws: WebSocket, request) => {
+        const id = (request?.url || '').match(/\?id=(\d+)/) ?. [1];
+
+        if (!id) {
+            ws.send(JSON.stringify(createPackage(
+                PackageTypes.Message,
+                'No lobby id provided'
+            )))
+            ws.close();
+            return;
+        }
+
         const lobby = Lobby.findById(id);
 
         if (lobby === undefined) {
-            ws.close(404);
+            ws.terminate();
+            console.log('Lobby #' + id + ' not found');
             return;
         }
 
@@ -23,7 +33,7 @@ app.get('/ws/:id', ( req, res ) => {
 
         console.log(`${request.socket.remoteAddress} joined lobby #${lobby.id}`);
 
-        ws.on('message', (message: string) => {
+        ws.on('message', (message: string, ...args) => {
             console.log(request.socket.remoteAddress + ' said: ' + message)
 
             lobby.participantList().forEach(p => {
@@ -32,6 +42,8 @@ app.get('/ws/:id', ( req, res ) => {
                     message.toString()
                 )))
             })
+
+            console.log(args)
         });
 
         ws.on('close', () => {
@@ -44,5 +56,7 @@ app.get('/ws/:id', ( req, res ) => {
 
             console.log('disconnected ' + request.socket.remoteAddress);
         });
-    })
+    });
+
+    console.log('Fin')
 });
